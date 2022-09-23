f666eb1880d7f0d3b67ebd92f1352311b0df6b71
karthikeyan k <105141303+KarthikeyanKanniappan@users.noreply.github.com>
2022-09-23 12:09:54 +0530

    envSecond commit

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ app.post("/", (req, res) => {
   res.send(final);
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("server listening on port 3000");
 });
