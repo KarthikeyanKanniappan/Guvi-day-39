f10a83ce3417f60490c578860e93e7af6cae3444
karthikeyan k <105141303+KarthikeyanKanniappan@users.noreply.github.com>
2022-09-23 11:53:40 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+let express = require("express");
+let app = express();
+const bodyParser = require("body-parser");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  let a = req.body.name1;
+  let b = req.body.name2;
+  let c = req.body.app1;
+  let d = req.body.app2;
+  let e = req.body.name3;
+  let final;
+  if (a && b && c && d && e !== undefined) {
+    final = `The Room ${e},is booked by ${a} on ${b} from ${c} to ${d} `;
+  } else {
+    final = "Enter a proper details";
+  }
+  res.send(final);
+});
+
+app.listen(3000, () => {
+  console.log("server listening on port 3000");
+});
